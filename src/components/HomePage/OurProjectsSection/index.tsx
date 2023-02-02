fe06dcc4a245a19ac4435aa7d9770db22bf673cf
TopengDev <topengdev@outlook.com>
2023-02-02 20:29:20 +0700

    add: OurTools section, responsive homepage sections

--- src/components/HomePage/OurProjectsSection/index.tsx
+++ src/components/HomePage/OurProjectsSection/index.tsx
@@ -3,18 +3,22 @@ import Tab from "./Tab";
 import CardProject from "./CardProject";
 import Badge from "./child/Badge";
 
-const OurProjectsSection: FC = () => {
+interface ICustomProps {
+  screenWidth: number;
+}
+
+const OurProjectsSection: FC<ICustomProps> = ({ screenWidth }) => {
   return (
-    <section className="min-h-fit w-full bg-base-500 py-10">
-      <h1 className="text-gradient-purple text-center text-5xl font-bold">
+    <section className="flex min-h-[100vh] w-full flex-col justify-center bg-base-500 py-10 xl:px-10">
+      <h1 className="text-gradient-purple text-center text-3xl font-bold lg:text-4xl xl:text-5xl">
         Our Projects
       </h1>
-      <div className="tabs items-center justify-center gap-x-3 py-8">
+      <div className="tabs items-center justify-center gap-x-3 py-8 text-xl">
         <Tab tab="All" isActive />
         <Tab tab="Ongoing" />
         <Tab tab="Done" />
       </div>
-      <div className="mx-10 grid grid-cols-2 gap-x-10 place-self-center">
+      <div className="mx-10 grid gap-x-10 gap-y-10 place-self-center px-20 lg:grid-cols-2 lg:px-0">
         <CardProject
           title="Moris - Fintech Application"
           desc="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
@@ -27,6 +31,7 @@ const OurProjectsSection: FC = () => {
               <Badge text="Mobile" />
             </>
           }
+          screenWidth={screenWidth}
         />
         <CardProject
           title="Moris - Fintech Application"
@@ -40,6 +45,7 @@ const OurProjectsSection: FC = () => {
               <Badge text="Mobile" />
             </>
           }
+          screenWidth={screenWidth}
         />
       </div>
     </section>
--- src/components/HomePage/TimelineSection/NodeItem.tsx
+++ src/components/HomePage/TimelineSection/NodeItem.tsx
@@ -1,37 +1,50 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-import { type FC } from "react";
 
 type NodeItemProps = {
   title: string;
   icon: JSX.Element;
   liIdx: number;
+  startColor: string;
 };
 
-const NodeItem: FC<NodeItemProps> = ({ title, icon, liIdx }) => {
+const NodeItem = ({ title, icon, liIdx, startColor }: NodeItemProps) => {
   const isEven = (num: number) => num % 2 === 0;
   // const IconTag = `${icon}` as keyof JSX.IntrinsicElements;
   return (
-    <div className={`item-${liIdx}`}>
-      {!isEven(liIdx) ? (
-        <div className={`flex items-center justify-end`}>
-          <p className="pr-3 text-3xl font-medium text-white">{title}</p>
-          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-t from-[#3F0741] to-[#8F008E]">
-            {/* <IconTag color="white" size={45} className="absolute" /> */}
-            {icon}
+    <>
+      <div className={`item-${liIdx}`}>
+        {!isEven(liIdx) ? (
+          <div className={`flex items-center justify-end`}>
+            <div className="mr-4 flex w-20 flex-col-reverse items-center gap-8 md:w-full md:flex-row md:gap-0">
+              <p className="pr-0 text-lg font-medium text-white md:pr-3 md:text-2xl xl:text-3xl">
+                {title}
+              </p>
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-t from-[#3F0741] to-[#8F008E] md:h-12 md:w-12 xl:h-14 xl:w-14">
+                {/* <IconTag color="white" size={45} className="absolute" /> */}
+                {icon}
+              </div>
+            </div>
+
+            <div className="from h-[0.2rem] w-8 bg-gradient-to-l from-[#ff1cf7] to-[#8F008E] md:w-12 xl:w-16" />
           </div>
-          <div className="from h-[0.2rem] w-16 bg-gradient-to-l from-[#ff1cf7] to-[#8F008E]" />
-        </div>
-      ) : (
-        <div className={`flex items-center justify-start`}>
-          <div className="from h-[0.2rem] w-16 bg-gradient-to-r from-[#ff1cf7] to-[#8F008E]" />
-          <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-t from-[#3F0741] to-[#8F008E]">
-            {/* <IconTag color="white" size={45} className="absolute" /> */}
-            {icon}
+        ) : (
+          <div className={`flex  items-center justify-start`}>
+            <div
+              className={`from h-[0.2rem] w-8 bg-gradient-to-r from-[#ff1cf7] to-[#8F008E] md:w-12 xl:w-16`}
+            />
+            <div className="ml-4 flex w-20 flex-col items-center gap-8 md:w-full  md:flex-row md:gap-0">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gradient-to-t from-[#3F0741] to-[#8F008E] md:h-12 md:w-12 xl:h-14 xl:w-14">
+                {/* <IconTag color="white" size={45} className="absolute" /> */}
+                {icon}
+              </div>
+              <p className="text-lg font-medium text-white md:pl-3 md:text-2xl xl:text-3xl">
+                {title}
+              </p>
+            </div>
           </div>
-          <p className="pl-3 text-3xl font-medium text-white">{title}</p>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
